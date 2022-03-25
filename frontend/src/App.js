61067b258270190fee10be64431ef5d5a76e311e
AnshulJ98 <anshulj@Zenbook.localdomain>
2022-03-24 20:02:09 -0400

    AKJanitorial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Heloo wrld
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+src="https://akjanitorial.ca/wp-content/uploads/2021/06/Thumbnail.png"
+            alt="Sample Brand Logo"
+            width="35"
+            className="align-top d-inline-block"
+            height="35"
+          />AKJanitorial
+        </Navbar.Brand>
+      </Navbar>
+
       </header>
+      <body>
+        <p>AKJanitorial</p>
+        AKJanitorial
+      </body>
     </div>
   );
 }
