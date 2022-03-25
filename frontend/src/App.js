bacfc5597a2d1a9b8d5a377fbdb9afe107794ffb
AnshulJ98 <anshulj@AnshulJoshi.localdomain>
2022-03-25 14:19:43 -0400

    bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">
-          <img
-src="https://akjanitorial.ca/wp-content/uploads/2021/06/Thumbnail.png"
-            alt="Sample Brand Logo"
-            width="35"
-            className="align-top d-inline-block"
-            height="35"
-          />AKJanitorial
-        </Navbar.Brand>
-      </Navbar>
-
+        Heloo wrld
       </header>
-      <body>
-        <p>AKJanitorial</p>
-        AKJanitorial
-      </body>
     </div>
   );
 }
